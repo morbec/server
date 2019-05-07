95117e683ec8ca7cae904d29b7f087114889394a
Fabio Morbec <morbecfm@gmail.com>
2019-05-07 16:29:53 +0200

    Initial import

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+    avatar: String,
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+)
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
